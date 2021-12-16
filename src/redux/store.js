482fcb16268359f3f76d0fa36fcd193348a576f7
Naveed ul Hassan Malik <naveed@appmakers.pk>
2021-12-16 19:27:55 +0500

    added logger middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 import videosReducer from "./modules/videosSlice";
 import playerReducer from "./modules/playerSlice";
+import {logger} from "redux-logger";
 
 export const store = configureStore({
     reducer: {
         videos: videosReducer,
         player: playerReducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 });
 
